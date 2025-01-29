b23dd4c7d38169550913fc1dd77876c6567a9f9a
David <67815728+wusunzu@users.noreply.github.com>
2025-01-29 14:57:14 +0800

    Update main.js

--- main.js
+++ main.js
@@ -30,3 +30,25 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800, // 初始宽度
+    height: 600, // 初始高度
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadURL('index.html')
+
+  // 监听窗口大小变化
+  win.on('resize', () => {
+    const [width, height] = win.getSize()
+    win.webContents.send('window-resized', { width, height })
+  })
+}
+
+app.whenReady().then(createWindow)
