6b715ded05b1dfcca0f7907989f40b1408077266
David <67815728+wusunzu@users.noreply.github.com>
2025-01-29 16:08:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const path = require('path')
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 900,
-    height: 800,
+    width: 900, // 初始宽度
+    height: 800, // 初始高度
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
@@ -13,6 +13,12 @@ function createWindow() {
   })
 
   win.loadFile('index.html')
+
+  // 监听窗口大小变化
+  win.on('resize', () => {
+    const [width, height] = win.getSize()
+    win.webContents.send('window-resized', { width, height })
+  })
 }
 
 app.whenReady().then(() => {
@@ -31,24 +37,3 @@ app.on('window-all-closed', () => {
   }
 })
 
-const { app, BrowserWindow } = require('electron')
-
-function createWindow() {
-  const win = new BrowserWindow({
-    width: 800, // 初始宽度
-    height: 600, // 初始高度
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-
-  win.loadURL('index.html')
-
-  // 监听窗口大小变化
-  win.on('resize', () => {
-    const [width, height] = win.getSize()
-    win.webContents.send('window-resized', { width, height })
-  })
-}
-
-app.whenReady().then(createWindow)
